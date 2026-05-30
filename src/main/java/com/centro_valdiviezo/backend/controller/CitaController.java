360ac21cd094122de206ed8624e3a6ffc28a397e
Jeison Gonzales <jgonzales17@gmail.com>
2026-05-30 06:58:05 -0500

    refactor: aislar CitaController de CitaRepository para cumplir SRP

--- src/main/java/com/centro_valdiviezo/backend/controller/CitaController.java
+++ src/main/java/com/centro_valdiviezo/backend/controller/CitaController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @RestController
 @RequestMapping("/api/cita")
 @CrossOrigin(origins = "*")
